aec36c386ff13991b2dfbc997179150b2594d5ce
Mooka <47282413+mooka007@users.noreply.github.com>
2023-03-16 18:40:50 +0100

    all the exercices

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-
+/**
+	 * main - A program that prints a line whit puts function
+	 * Return: 0 (Success)
+	 */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
