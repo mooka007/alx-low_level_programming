6bf69bdfbf07570a890989ec2f9f6dceafcd692c
mooka007 <mooka3084@gmail.com>
2023-03-17 14:35:04 -0700

    first task and README

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int n;
+
+	scanf("%d", &n);
+
+	if (n > 0)
+	{
+	printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+	printf("%d is zero\n", n);
+	}
+	else
+	{
+	printf("%d is negative\n", n);
+	}
+	return (0);
+}
