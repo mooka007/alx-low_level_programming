361535cdb205173656d7d72079e3f4d29fc50674
mooka007 <mooka3084@gmail.com>
2023-03-17 14:55:55 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+/**
+* main - Entry point
+*
+* Description: 'the program's description'
+*
+* Return: Always 0 (Success)
+*/
+
+	int main(void)
+{
+
+	int ch = 97;
+	int CH = 65;
+
+	while (ch <= 122)
+	{
+		putchar (ch);
+		ch++;
+	}
+	while (CH <= 90)
+	{
+		putchar (CH);
+		CH++;
+
+	}
+		putchar('\n');
+	return (0);
+
+}
