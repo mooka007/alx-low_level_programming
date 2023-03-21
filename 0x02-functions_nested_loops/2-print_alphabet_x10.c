306b02b8e24efcecc20bcf90434b9773d496db45
mooka007 <mooka3084@gmail.com>
2023-03-21 14:38:01 -0700

    update

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * Print_alphabet_x10 - repeats the print_alphabet 10 times.
+ * Return;
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char c = 'a';
+
+	for (i = 0; i < 10; i++)
+	{
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+	}
+}
