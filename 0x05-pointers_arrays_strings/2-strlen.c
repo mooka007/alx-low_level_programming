398c4c2da8c8884ad49d7f08912b483ae6761d74
mooka007 <mooka3084@gmail.com>
2023-03-27 14:17:48 -0700

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *_strlen - return the length of a string
+ * Owned By Bwave
+ * @s: char to check
+ * Description: this will return the length of a string
+ * Return: 0 is success
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while ( s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
