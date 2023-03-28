d7017dbbe94acf2b0538cfc386bd647ca04ef805
mooka007 <mooka3084@gmail.com>
2023-03-27 18:47:33 -0700

    puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * puts2 - prints one char out of 2 of a string
+ * followed by a new line
+ * @str: string to print the chars from
+ */
+
+void	puts2(char *str)
+{
+	int x;
+
+	for (x = 0; str[x] != '\0'; x++)
+		if (x % 2 == 0)
+			_putchar(str[x]);
+		_putchar('\n');
+}
