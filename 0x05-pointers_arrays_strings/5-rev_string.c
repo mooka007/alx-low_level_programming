e3239cd62343445761f41846b36c10b87f45cdf9
mooka007 <mooka3084@gmail.com>
2023-03-27 18:33:33 -0700

    rev string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: string to be reversed
+ */
+
+void rev_string(char *s)
+{
+	char tmp;
+	int i, x, y;
+
+	x = 0;
+	y = 0;
+
+	while (s[x] != '\0')
+	{
+		x++;
+	}
+
+	y = x - 1;
+
+	for (i = 0; i < x / 2; i++)
+	{
+		tmp = s[i];
+		s[i] = s[y];
+		s[y] = tmp;
+	}
+}
