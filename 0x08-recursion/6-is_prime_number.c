e2434208c8727c64264611e562d3454c9c1cdd87
mooka007 <mooka3084@gmail.com>
2023-04-05 20:02:15 -0700

    is prime

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _detects - detects to see if number is prime
+ * @x:integer
+ * @y:integer
+ * Return:integer
+ */
+int _detects(int x, int y)
+{
+	if (y < 2 || y % x == 0)
+	{
+		return (0);
+	}
+	else if (x > y / 2)
+	{
+		return (1);
+	}
+	else
+	{
+		return (_detects(a + 1, b));
+	}
+}
+
+/**
+ * is_prime_number - calculates if a number is prime recursively
+ * @n:integer
+ * Return:integer
+ */
+
+int is_prime_number(int n)
+{
+	if (n == 2)
+	{
+		return (1);
+	}
+	return (_detects(2, n));
+}
