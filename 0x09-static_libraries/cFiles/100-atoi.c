fc8c4b505a070e4258a147227ada60746e5e26d0
mooka007 <mooka3084@gmail.com>
2023-04-08 07:31:09 -0700

    we still need 7 functions to finish the library

--- 0x09-static_libraries/cFiles/100-atoi.c
+++ 0x09-static_libraries/cFiles/100-atoi.c
@@ -1,4 +1,31 @@
 #include "main.h"
 
 
+int _atoi(char *s)
+{
+	int i = 0;
+	int nbr = 0;
+	int sign = 1;
+
+	while ((s[i] == '\t') || (s[i] == '\n') || (s[i] == '\r') || (s[i] == '\v') || (s[i] == ' ') || (s[i] == '\f'))
+	{
+		i++;
+	}
+	while (s[i] == '+' || s[i] == '-')
+	{
+		if (s[i] == '-')
+		{
+			sign = sign * -1;
+		}
+		i++;
+	}
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		nbr = nbr * 10 + (s[i] - '0');
+		i++;
+	}
+	nbr = sign * nbr;
+	return (nbr);
+}
+
 
