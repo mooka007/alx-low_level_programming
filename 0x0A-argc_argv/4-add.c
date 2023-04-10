393b56e8292ddd6e4cdcdd24798b76d6b2f5545d
mooka007 <mooka3084@gmail.com>
2023-04-10 15:04:30 -0700

    add positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - program adds positive numbers
+ * Owned By MoOka
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: sum of numbers
+ */
+
+int	main(int argc, char *argv[])
+{
+	int num;
+	int sum = 0;
+	int i, j;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j]; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		
+		 num = atoi(argv[i]);
+		
+		if (num > 0)
+		{
+			sum += num;
+		}
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+	
+}
