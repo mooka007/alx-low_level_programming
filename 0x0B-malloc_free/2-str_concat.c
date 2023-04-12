d2a4ca29e43cda349942e172d45f5b9aa218d6d9
mooka007 <mooka3084@gmail.com>
2023-04-11 19:55:13 -0700

    concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,50 +1,42 @@
 #include "main.h"
 #include <stdlib.h>
-
 /**
- * str_concat - a function that concatenates two string
- * owned by MoOka
+ * str_concat - Program thats concatenates two strings.
+ * owned By MoOka
  * @s1: first string
  * @s2: second string
  * Return: pointer of an array of chars
  */
-
-char	*str_concat(char *s1, char *s2)
+char *str_concat(char *s1, char *s2)
 {
-	char *concat
-	unsigned int a = 0, b = 0, c, d;
+	char *concat;
+	unsigned int i, j, k, limit;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
 
-	while (s1[a] != '\0')
-	{
-		a++;
-	}
-	while (s2[b] != '\0')
-	{
-		b++;
-	}
-	concat = mallow(sizeof(char) * (a + b + 1));
+	for (i = 0; s1[i] != '\0'; i++)
+		;
+
+	for  (j = 0; s2[j] != '\0'; j++)
+		;
+
+	concat = malloc(sizeof(char) * (i + j + 1));
 
 	if (concat == NULL)
 	{
+		free(concat);
 		return (NULL);
 	}
-	for (c = 0; k < i; k++)
-	{
-		concat[c] s1[k];
-	}
-	d = b;
-	for (b = 0; b <= d; c++, b++)
-	{
-		concat[c] = s2[b];
-	}
+
+	for  (k = 0; k < i; k++)
+		concat[k] = s1[k];
+
+	limit = j;
+	for (j = 0; j <= limit; k++, j++)
+		concat[k] = s2[j];
+
 	return (concat);
 }
