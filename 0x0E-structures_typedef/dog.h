649bf905a85362f40b2394ea519ee430b0edd122
mooka007 <mooka3084@gmail.com>
2023-04-17 08:34:04 -0700

    struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - struct define dog info
+ * Owned By MoOka
+ * @name:  name of the dog
+ * @age: age of the dog
+ * @owner: owner
+ * Return: Store different data types
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
