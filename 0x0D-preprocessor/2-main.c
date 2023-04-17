64c22a7e4b623993967639a2b865198b75236837
mooka007 <mooka3084@gmail.com>
2023-04-16 22:00:06 -0700

    2-main.c __FILE__

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - program prints the name of the file it was compiled from
+ * Owned By MoOka
+ * Return: 0
+ */
+
+int	main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
