91c7122c766cf9e6f9ebc0f34156b3f93995890d
mooka007 <mooka3084@gmail.com>
2023-04-19 15:30:24 -0700

    print name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,7 +3,8 @@
 /**
  * print_name - function that writes a name
  * Owned By MoOka
- * @f: a pointer to a function 
+ * @f: a pointer to a function
+ * @name: a pointer to a null-terminated string containing a name
  */
 
 void	print_name(char *name, void (*f)(char *))
