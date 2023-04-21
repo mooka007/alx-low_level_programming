7157af369bb0c5ac216503cf819a61fd2cbc3261
mooka007 <mooka3084@gmail.com>
2023-04-20 18:17:04 -0700

    print string

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,38 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_string - prints strings
+ * Owned By MoOka
+ * @separator: String to be printed between strings
+ * @n: number of strings passed 
+ * @...: A variable number
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	char *s;
+	unsigned int i;
+	va_list op;
+
+	va_start(op, n);
+	if (separator == NULL)
+		separator = "";
+	for (i = 0; i < n; i++)
+	{
+
+		s = va_arg(op, char*);
+		if (s == NULL)
+		{
+			s = "(nil)";
+		}
+		printf("%s", s);
+		if (i < n - 1)
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+	va_end(op);
+}
