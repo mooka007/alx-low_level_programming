e7e9ff7a2ed29b9c43107fe599b8b34a5a451b9c
mooka007 <mooka3084@gmail.com>
2023-04-27 15:21:43 -0700

    before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * first - function that prints before the main
+ * Owned By MoOka
+ * Return: nothing
+ */
+
+void __attribute__ ((constructor)) first()
+{
+	    printf("You're beat! and yet, you must allow,\n");
+	    printf("I bore my house upon my back!\n");
+}
+
