3ae5ce298a796972fcb65adc74e5cf0a5b5eeeb3
mooka007 <mooka3084@gmail.com>
2023-04-30 12:59:42 -0700

    add node int

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+/**
+ * add_nodeint_end - function that adds a new node at
+ * the end of a listint_t list
+ * Owned By Mooka
+ * @head: pointer to the address
+ * @n: integer
+ * Return: the address of the new element
+ */
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *mall, *x;
+
+	mall = malloc(sizeof(listint_t));
+	if (mall == NULL)
+		return (NULL);
+	mall->n = n;
+	mall->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = mall;
+	}
+	else
+	{
+		x = *head;
+		while (x->next != NULL)
+		{
+			x = x->next;
+			x->next = mall;
+		}
+	}
+	return (*head);
+}
