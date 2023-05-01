7a8438631698bc33c24cd7f1f918439087cd77ce
mooka007 <mooka3084@gmail.com>
2023-05-01 05:38:50 -0700

    free listint2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - function that frees a listint_t list
+ * Owned By MoOka
+ * @head: pointer to the header
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *x;
+
+	while (*head != NULL)
+	{
+		x = *head->next;
+		free(*head);
+		*head = x;
+	}
+	*head = NULL;
+}
