1252ddfdd67046e9f55d67b849d740beeea84345
mooka007 <mooka3084@gmail.com>
2023-05-02 08:57:36 -0700

    suim listint

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+
+/**
+* insert_nodeint_at_index - function that inserts a node at a given position
+* Owned By MoOka
+* @head: pointer to the address
+* @idx: index
+* @n: integer
+* Return: 0.
+*/
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *new_node, *current_node;
+	unsigned int i;
+
+	if (head == NULL)
+		return (NULL);
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
+	current_node = *head;
+	for (i = 0; i < idx - 1 && current_node != NULL; i++)
+	{
+		current_node = current_node->next;
+	}
+	if (current_node == NULL)
+		return (NULL);
+	new_node->next = current_node->next;
+	current_node->next = new_node;
+	return (new_node);
+}
