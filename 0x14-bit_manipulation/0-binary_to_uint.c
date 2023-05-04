8ddcda9fed4522dc4ce5fcd281d6d8a9826d9d6f
mooka007 <mooka3084@gmail.com>
2023-05-04 16:57:08 -0700

    0-binary_to_uint

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,19 +13,25 @@ unsigned int binary_to_uint(const char *b)
 
 	unsigned int dec_val = 0;
 	int i;
-
+	
 	if (!b)
 		return (0);
 
 	for (i = 0; b[i] != '\0'; i++)
-
-		if (b[i]  != '0' && b[i] != '1')
+	{
+		if (b[i] != '0' && b[i] != '1')
+		{
 			return (0);
+		}
+	}
+
 	for (i = 0; b[i] != '\0'; i++)
 	{
 		dec_val *= 2;
 		if (b[i] == '1')
+		{
 			dec_val += 1;
+		}
 	}
 	return (dec_val);
 }
