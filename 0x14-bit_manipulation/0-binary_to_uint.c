d4cf4035be9f475962628c73820cf52082df9a3a
mooka007 <mooka3084@gmail.com>
2023-05-04 17:15:23 -0700

    0-binary_to_uint

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,30 +1,28 @@
 #include "main.h"
-
 /**
  * binary_to_uint - Entry Point
- * Owned By MoOka
  * @b: const char
  * Return: 0
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int conv = 0;
-	int x = 1, i = 0;
+unsigned int conv = 0;
+int x = 1, i = 0;
 
-	if (b == NULL)
-		return (EXIT_FAILURE);
-	while (b[i + 1])
-	{
-		if (b[i] != '0' && b[i] != '1')
-		return (0);
-		i++;
-	}
-	while (i >= 0)
-	{
-		conv += ((b[i] - '0') * x);
-		x *= 2;
-		i--;
-	}
+if (b == NULL)
+return (0);
+while (b[i + 1])
+{
+if (b[i] != '0' && b[i] != '1')
+return (0);
+i++;
+}
+while (i >= 0)
+{
+conv += ((b[i] - '0') * x);
+x *= 2;
+i--;
+}
 
-	return (conv);
+return (conv);
 }
