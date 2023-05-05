c25af3486779b3049af81a36cd0661cd553016c8
mooka007 <mooka3084@gmail.com>
2023-05-04 17:20:29 -0700

    0-binary_to_uint

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * binary_to_uint - function that converts a binary number to an unsigned int
+ * owned By Mooka
+ * @b: const char
+ * Return: 0
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int conv = 0;
+	int x = 1, i = 0;
+
+	if (b == NULL)
+		return (0);
+
+	while (b[i + 1])
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+		i++;
+	}
+	while (i >= 0)
+	{
+		conv += ((b[i] - '0') * x);
+		x *= 2;
+		i--;
+	}
+	return (conv);
+}
