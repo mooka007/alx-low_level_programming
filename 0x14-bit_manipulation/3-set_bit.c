defe5da43cb98226ca1bdd05237982101f68ab49
mooka007 <mooka3084@gmail.com>
2023-05-04 17:52:39 -0700

    set bit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
 /**
- * set_bit - Sets the value of a bit at a given index.
- * @n: A pointer to the bit.
- * @index: The index start at 0.
- *
- * Return: If an error occurs - -1.
- *         Otherwise - 1.
+ * set_bit - function that sets the value of a bit to 1 at a given index
+ * Owned By MoOka
+ * @n: pointer 
+ * @index: index
+ * Return: If an error occurs - -1
  */
+
 int set_bit(unsigned long int *n, unsigned int index)
 {
-if (index >= (sizeof(unsigned long int) * 8))
-return (-1);
-
-*n ^= (1 << index);
-
-return (1);
+	if (index >= (sizeof(unsigned long int) * 8))
+	{
+		return (-1);
+	}
+	*n ^= (1 << index);
+	return (1);
 }
