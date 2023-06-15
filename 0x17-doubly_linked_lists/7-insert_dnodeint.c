cb95325209e02ffc90b7fb52d78e7410f1c182d1
mooka007 <mooka3084@gmail.com>
2023-06-15 16:43:43 -0700

    7 - dlistint

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,4 +1,4 @@
-nclude "lists.h"
+#include "lists.h"
 
 /**
  * insert_dnodeint_at_index - inserts a new node at
@@ -49,6 +49,5 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			i++;
 		}
 	}
-
 	return (new);
 }
