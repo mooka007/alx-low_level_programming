b23d83f7ada792480b123e39c24aa2eefad5a331
mooka007 <mooka3084@gmail.com>
2023-06-15 15:37:15 -0700

    print dlistint

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - minimum of coins to make change
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: amount of money
+ */
+
+int	main(int argc, char *argv[])
+{
+
+
