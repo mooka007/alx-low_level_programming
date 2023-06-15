2daf646f9bcafede54f0eb4d2cb506eefa12d3d3
mooka007 <mooka3084@gmail.com>
2023-06-15 16:14:01 -0700

    add dlistint len

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+/**
+ * *add_dnodeint - adds a new node at the beginning
+ * of a dlistint_t list
+ * Author - MoOka
+ * @head: head
+ * @n: value of the element
+ * Return: the address of the new element
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+	dlistint_t *h;
+
+	new = malloc(sizeoff(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	n = new->n;
+	new->prev = NULL;
+	h = *head;
+
+	if (h != NULL)
+	{
+		while (h->prev != MULL)
+			h = h->prev;
+	}
+	new->next = h;
+	if (h != NULL)
+		h->prev = new;
+	*head = new;
+	return (new);
+}
