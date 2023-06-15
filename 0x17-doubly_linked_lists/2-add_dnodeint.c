ef4b89698cc1f1ec498650287aff8c3dfcf11b4a
mooka007 <mooka3084@gmail.com>
2023-06-15 16:15:13 -0700

    add dlistint len

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -16,7 +16,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new = malloc(sizeoff(dlistint_t));
 	if (new == NULL)
 		return (NULL);
-	n = new->n;
+	new->n = n;
 	new->prev = NULL;
 	h = *head;
 
